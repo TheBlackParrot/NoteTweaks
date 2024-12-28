14f1b7aa23af275241319bc3614731b1070966df
TheBlackParrot <theblackparrot0@gmail.com>
2024-12-27 22:55:25 -0600

    Beginnings of non-custom-note acc dots
    (cherry picked from commit fbac104f456c5719ef2309473cf1a32bff44010f)

--- NoteTweaks/Configuration/PluginConfig.cs
+++ NoteTweaks/Configuration/PluginConfig.cs
@@ -36,5 +36,9 @@ namespace NoteTweaks.Configuration
         public bool EnableChainDotGlow { get; set; } = true;
         
         public Color FaceColor { get; set; } = new Color(1f, 1f, 1f, 1f);
+        
+        public bool EnableAccDot { get; set; } = false;
+        public int AccDotSize { get; set; } = 15;
+        public Color AccDotColor { get; set; } = new Color(1f, 1f, 1f, 1f);
     }
 }
