e74a268d6b2baab87aff90cb4dbf1e4a2dd0bf92
TheBlackParrot <theblackparrot0@gmail.com>
2024-12-28 23:12:19 -0600

    Clamp settings here too because i'm paranoid lmao

--- NoteTweaks/Patches/NoteArrowPatch.cs
+++ NoteTweaks/Patches/NoteArrowPatch.cs
@@ -91,6 +91,7 @@ namespace NoteTweaks.Patches
             internal static void Postfix(in GameplayModifiers gameplayModifiers)
             {
                 _gameplayModifiers = gameplayModifiers;
+                Plugin.ClampSettings();
             }
         }
 
