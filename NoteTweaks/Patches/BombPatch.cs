7c414dc2309c6c93b727c80e26ca916f07626949
TheBlackParrot <theblackparrot0@gmail.com>
2025-01-09 21:05:37 -0600

    i give up on chroma for now

--- NoteTweaks/Patches/BombPatch.cs
+++ NoteTweaks/Patches/BombPatch.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 namespace NoteTweaks.Patches
 {
     [HarmonyPatch(typeof(BombNoteController), "Init")]
-    [HarmonyAfter("aeroluna.Chroma")]
     internal class BombPatch
     {
         private static readonly int Color0 = Shader.PropertyToID("_SimpleColor");
         
+        [HarmonyPriority(int.MaxValue)]
         internal static void Postfix(BombNoteController __instance)
         {
             if (!Plugin.Config.Enabled || NotePhysicalTweaks._autoDisable)
@@ -19,6 +19,32 @@ namespace NoteTweaks.Patches
             
             float colorScale = 1.0f + Plugin.Config.BombColorBoost;
             Color bombColor = Plugin.Config.BombColor * colorScale;
+            
+            // i'll figure this out one day :clueless:
+            /*Type objectColorizeType = AccessTools.TypeByName("Chroma.Colorizer.BombColorizerManager");
+            if (objectColorizeType == null)
+            {
+                if (__instance.transform.GetChild(0).TryGetComponent(out Renderer bombRenderer))
+                {
+                    bombRenderer.sharedMaterial = Materials.BombMaterial;
+                    bombRenderer.sharedMaterial.SetColor(Color0, bombColor);
+                }
+
+                if (__instance.gameObject.TryGetComponent(out MaterialPropertyBlockController materialPropertyBlockController))
+                {
+                    materialPropertyBlockController.materialPropertyBlock.SetColor(Color0, bombColor);
+                    materialPropertyBlockController.ApplyChanges();   
+                }
+            }
+            else
+            {
+                MethodInfo colorizeMethod = AccessTools.Method(objectColorizeType, "Colorize");
+                if (colorizeMethod != null)
+                {
+                    object colorizerObject = AccessTools.CreateInstance(objectColorizeType);
+                    colorizeMethod.Invoke(colorizerObject, new object[] { __instance, bombColor } );
+                }
+            }*/
             
             if (__instance.transform.GetChild(0).TryGetComponent(out Renderer bombRenderer))
             {
