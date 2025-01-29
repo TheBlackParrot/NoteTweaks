52ede93eaaca8ef6a7aada62b73ac12bdc1a33bd
TheBlackParrot <theblackparrot0@gmail.com>
2025-01-28 21:38:18 -0600

    mod settings sync progress
    (cherry picked from commit 61cd245c8913229405fd0651ae4fab5162cbc535)

---
+++ NoteTweaks/UI/ModSettingsViewController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel;
+using BeatSaberMarkupLanguage.GameplaySetup;
+using NoteTweaks.Configuration;
+using Zenject;
+
+namespace NoteTweaks.UI
+{
+    internal class ModSettingsViewController : IInitializable, IDisposable, INotifyPropertyChanged
+    {
+        private static PluginConfig _config;
+        private readonly GameplaySetup _gameplaySetup;
+        private readonly GameplaySetupViewController _gameplaySetupViewController;
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private ModSettingsViewController(PluginConfig config, GameplaySetup gameplaySetup, GameplaySetupViewController gameplaySetupViewController)
+        {
+            _config = config;
+            _gameplaySetup = gameplaySetup;
+            _gameplaySetupViewController = gameplaySetupViewController;
+        }
+        
+        public void Initialize()
+        {
+            _gameplaySetup.AddTab("NoteTweaks", "NoteTweaks.UI.BSML.SideSettings.bsml", this);
+            _gameplaySetupViewController.didActivateEvent += GameplaySetupViewController_DidActivateEvent;
+        }
+
+        public void Dispose()
+        {
+            _gameplaySetup.RemoveTab("NoteTweaks");
+            _gameplaySetupViewController.didActivateEvent -= GameplaySetupViewController_DidActivateEvent;
+        }
+
+        private void ParentControllerActivated()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Enabled)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DisableIfNoodle)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FixDotsIfNoodle)));
+        }
+        
+        private void GameplaySetupViewController_DidActivateEvent(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+        {
+            ParentControllerActivated();
+        }
+
+        protected bool Enabled
+        {
+            get => _config.Enabled;
+            set
+            {
+                _config.Enabled = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Enabled)));
+            }
+        }
+
+        protected bool DisableIfNoodle
+        {
+            get => _config.DisableIfNoodle;
+            set
+            {
+                _config.DisableIfNoodle = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DisableIfNoodle)));
+            }
+        }
+
+        protected bool FixDotsIfNoodle
+        {
+            get => _config.FixDotsIfNoodle;
+            set
+            {
+                _config.FixDotsIfNoodle = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FixDotsIfNoodle)));
+            }
+        }
+    }
+}
