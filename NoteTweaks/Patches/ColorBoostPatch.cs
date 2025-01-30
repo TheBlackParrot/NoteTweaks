1d727746185f6400792ed689c82902783b76324b
TheBlackParrot <theblackparrot0@gmail.com>
2025-01-29 20:12:33 -0600

    seeing if this avoids some rare color issues
    (cherry picked from commit 7771d0a2646225e06f1d7240578ed7e6c052c807)

--- NoteTweaks/Patches/ColorBoostPatch.cs
+++ NoteTweaks/Patches/ColorBoostPatch.cs
@@ -118,12 +118,17 @@ namespace NoteTweaks.Patches
         // ReSharper disable once InconsistentNaming
         private static void InitColorInfoPatch(StandardLevelScenesTransitionSetupDataSO __instance)
         {
-            
             if (!Plugin.Config.Enabled)
             {
                 return;
             }
 
+            if (Plugin.Config.ColorBoostLeft == 0f && Plugin.Config.ColorBoostRight == 0f)
+            {
+                return;
+            }
+
+            __instance.usingOverrideColorScheme = true;
             __instance.colorScheme = PatchColors(__instance.colorScheme);
         }
 
