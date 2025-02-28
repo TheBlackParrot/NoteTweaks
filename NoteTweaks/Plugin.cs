df4859e17d91fd477392a6e6608a05565de480f4
TheBlackParrot <theblackparrot0@gmail.com>
2025-02-27 23:41:33 -0600

    cap bomb mesh settings, increase max mesh stuff to 48

--- NoteTweaks/Plugin.cs
+++ NoteTweaks/Plugin.cs
@@ -61,6 +61,9 @@ namespace NoteTweaks
             
             PluginConfig.Instance.FogScale = Mathf.Max(PluginConfig.Instance.FogScale, 0.0f);
             PluginConfig.Instance.FogHeightScale = Mathf.Max(PluginConfig.Instance.FogHeightScale, 0.0f);
+
+            PluginConfig.Instance.BombMeshStacks = Math.Max(Math.Min(PluginConfig.Instance.BombMeshStacks, 48), 1);
+            PluginConfig.Instance.BombMeshSlices = Math.Max(Math.Min(PluginConfig.Instance.BombMeshSlices, 48), 1);
         }
 
         [Init]
