ef49415a7ecea873ef36b843e82a17fc2bc9c596
TheBlackParrot <theblackparrot0@gmail.com>
2025-07-04 06:20:14 -0500

    move those mesh functions over there

--- NoteTweaks/Managers/OutlineManager.cs
+++ NoteTweaks/Managers/OutlineManager.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using IPA.Utilities;
 using NoteTweaks.Configuration;
+using NoteTweaks.Utils;
 using UnityEngine;
 #if !PRE_V1_39_1
 using UnityEngine.Rendering;
@@ -9,78 +10,9 @@ using UnityEngine.Rendering;
 
 namespace NoteTweaks.Managers
 {
-#if !PRE_V1_39_1
-    internal static class MeshExtensions
-    {
-        internal static Mesh Invert(this Mesh mesh)
-        {
-            Vector3[] normals = mesh.normals;
-            for(int i = 0; i < normals.Length; i++)
-            {
-                normals[i] = -1 * normals[i];
-            }
-            mesh.normals = normals;
-
-            int[] triangles = mesh.triangles;
-            for (int i = 0; i < triangles.Length; i+=3)
-            {
-                (triangles[i], triangles[i + 2]) = (triangles[i + 2], triangles[i]);
-            }
-
-            mesh.triangles = triangles;
-            return mesh;
-        }
-    }
-#endif
-    
     internal abstract class Outlines
     {
         private static PluginConfig Config => PluginConfig.Instance;
-        
-#if !PRE_V1_39_1
-        // https://discussions.unity.com/t/reading-meshes-at-runtime-that-are-not-enabled-for-read-write/804189/8
-        private static Mesh MakeReadableMeshCopy(Mesh nonReadableMesh)
-        {
-            Mesh meshCopy = new Mesh
-            {
-                indexFormat = nonReadableMesh.indexFormat
-            };
-
-            // Handle vertices
-            GraphicsBuffer verticesBuffer = nonReadableMesh.GetVertexBuffer(0);
-            int totalSize = verticesBuffer.stride * verticesBuffer.count;
-            byte[] data = new byte[totalSize];
-            verticesBuffer.GetData(data);
-            meshCopy.SetVertexBufferParams(nonReadableMesh.vertexCount, nonReadableMesh.GetVertexAttributes());
-            meshCopy.SetVertexBufferData(data, 0, 0, totalSize);
-            verticesBuffer.Release();
-
-            // Handle triangles
-            meshCopy.subMeshCount = nonReadableMesh.subMeshCount;
-            GraphicsBuffer indexesBuffer = nonReadableMesh.GetIndexBuffer();
-            int tot = indexesBuffer.stride * indexesBuffer.count;
-            byte[] indexesData = new byte[tot];
-            indexesBuffer.GetData(indexesData);
-            meshCopy.SetIndexBufferParams(indexesBuffer.count, nonReadableMesh.indexFormat);
-            meshCopy.SetIndexBufferData(indexesData, 0, 0, tot);
-            indexesBuffer.Release();
-
-            // Restore submesh structure
-            uint currentIndexOffset = 0;
-            for (int i = 0; i < meshCopy.subMeshCount; i++)
-            {
-                uint subMeshIndexCount = nonReadableMesh.GetIndexCount(i);
-                meshCopy.SetSubMesh(i, new SubMeshDescriptor((int)currentIndexOffset, (int)subMeshIndexCount));
-                currentIndexOffset += subMeshIndexCount;
-            }
-
-            // Recalculate normals and bounds
-            meshCopy.RecalculateNormals();
-            meshCopy.RecalculateBounds();
-
-            return meshCopy;
-        }
-#endif
         
         private static Mesh _defaultNoteMesh;
         public static Mesh InvertedNoteMesh;
@@ -99,7 +31,7 @@ namespace NoteTweaks.Managers
                 _defaultNoteMesh = mesh;
                 InvertedNoteMesh = _defaultNoteMesh;
 #else
-                _defaultNoteMesh = MakeReadableMeshCopy(mesh);
+                _defaultNoteMesh = Utils.Meshes.MakeReadableMeshCopy(mesh);
                 InvertedNoteMesh = _defaultNoteMesh.Invert();
 #endif
             }
@@ -112,7 +44,7 @@ namespace NoteTweaks.Managers
                 _defaultChainMesh = mesh;
                 InvertedChainMesh = _defaultChainMesh;
 #else
-                _defaultChainMesh = MakeReadableMeshCopy(mesh);
+                _defaultChainMesh = Utils.Meshes.MakeReadableMeshCopy(mesh);
                 InvertedChainMesh = _defaultChainMesh.Invert();
 #endif
             }
@@ -125,7 +57,7 @@ namespace NoteTweaks.Managers
                 _defaultChainHeadMesh = mesh;
                 InvertedChainHeadMesh = _defaultChainHeadMesh;
 #else
-                _defaultChainHeadMesh = MakeReadableMeshCopy(mesh);
+                _defaultChainHeadMesh = Utils.Meshes.MakeReadableMeshCopy(mesh);
                 InvertedChainHeadMesh = _defaultChainHeadMesh.Invert();
 #endif
             }
@@ -138,7 +70,7 @@ namespace NoteTweaks.Managers
                 _defaultBombMesh = mesh;
                 InvertedBombMesh = _defaultBombMesh;
 #else
-                _defaultBombMesh = MakeReadableMeshCopy(mesh);
+                _defaultBombMesh = Utils.Meshes.MakeReadableMeshCopy(mesh);
                 InvertedBombMesh = _defaultBombMesh.Invert();
 #endif
             }
--- NoteTweaks/Utils/Utils.cs
+++ NoteTweaks/Utils/Utils.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 //using AssetBundleLoadingTools.Models.Shaders;
 //using AssetBundleLoadingTools.Utilities;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace NoteTweaks.Utils
 {
@@ -282,6 +283,70 @@ namespace NoteTweaks.Utils
 
             return mesh;
         }
+        
+#if !PRE_V1_39_1
+        internal static Mesh Invert(this Mesh mesh)
+        {
+            Vector3[] normals = mesh.normals;
+            for(int i = 0; i < normals.Length; i++)
+            {
+                normals[i] = -1 * normals[i];
+            }
+            mesh.normals = normals;
+
+            int[] triangles = mesh.triangles;
+            for (int i = 0; i < triangles.Length; i+=3)
+            {
+                (triangles[i], triangles[i + 2]) = (triangles[i + 2], triangles[i]);
+            }
+
+            mesh.triangles = triangles;
+            return mesh;
+        }
+        
+        // https://discussions.unity.com/t/reading-meshes-at-runtime-that-are-not-enabled-for-read-write/804189/8
+        public static Mesh MakeReadableMeshCopy(Mesh nonReadableMesh)
+        {
+            Mesh meshCopy = new Mesh
+            {
+                indexFormat = nonReadableMesh.indexFormat
+            };
+
+            // Handle vertices
+            GraphicsBuffer verticesBuffer = nonReadableMesh.GetVertexBuffer(0);
+            int totalSize = verticesBuffer.stride * verticesBuffer.count;
+            byte[] data = new byte[totalSize];
+            verticesBuffer.GetData(data);
+            meshCopy.SetVertexBufferParams(nonReadableMesh.vertexCount, nonReadableMesh.GetVertexAttributes());
+            meshCopy.SetVertexBufferData(data, 0, 0, totalSize);
+            verticesBuffer.Release();
+
+            // Handle triangles
+            meshCopy.subMeshCount = nonReadableMesh.subMeshCount;
+            GraphicsBuffer indexesBuffer = nonReadableMesh.GetIndexBuffer();
+            int tot = indexesBuffer.stride * indexesBuffer.count;
+            byte[] indexesData = new byte[tot];
+            indexesBuffer.GetData(indexesData);
+            meshCopy.SetIndexBufferParams(indexesBuffer.count, nonReadableMesh.indexFormat);
+            meshCopy.SetIndexBufferData(indexesData, 0, 0, tot);
+            indexesBuffer.Release();
+
+            // Restore submesh structure
+            uint currentIndexOffset = 0;
+            for (int i = 0; i < meshCopy.subMeshCount; i++)
+            {
+                uint subMeshIndexCount = nonReadableMesh.GetIndexCount(i);
+                meshCopy.SetSubMesh(i, new SubMeshDescriptor((int)currentIndexOffset, (int)subMeshIndexCount));
+                currentIndexOffset += subMeshIndexCount;
+            }
+
+            // Recalculate normals and bounds
+            meshCopy.RecalculateNormals();
+            meshCopy.RecalculateBounds();
+
+            return meshCopy;
+        }
+#endif
     }
 
     internal static class Vectors
