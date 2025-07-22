c213d48de695537ad038641453d076c17b3a575c
TheBlackParrot <theblackparrot0@gmail.com>
2025-07-21 22:22:16 -0500

    parrot sweetie it's doing the same thing, refactor that

--- NoteTweaks/Patches/NoteArrowPatch.cs
+++ NoteTweaks/Patches/NoteArrowPatch.cs
@@ -92,17 +92,33 @@ namespace NoteTweaks.Patches
             return hasRequirement;
         }
 
-        // thanks BeatLeader
         [HarmonyPatch]
-        internal class StandardLevelScenesTransitionSetupDataPatch
+        internal class LevelScenesTransitionPatches
         {
-            [UsedImplicitly]
-            static MethodInfo TargetMethod() => AccessTools.FirstMethod(typeof(StandardLevelScenesTransitionSetupDataSO),
-                m => m.Name == nameof(StandardLevelScenesTransitionSetupDataSO.Init) &&
-                     m.GetParameters().All(p => p.ParameterType != typeof(IBeatmapLevelData)));
-            
-            // ReSharper disable once InconsistentNaming
-            internal static void Postfix(StandardLevelScenesTransitionSetupDataSO __instance, in GameplayModifiers gameplayModifiers)
+            private static bool PrefixCall()
+            {
+                if (!Config.Enabled)
+                {
+                    return true;
+                }
+
+                Managers.Meshes.UpdateSphereMesh(Config.BombMeshSlices, Config.BombMeshStacks,
+                    Config.BombMeshSmoothNormals, Config.BombMeshWorldNormals);
+
+                UnityMainThreadTaskScheduler.Factory.StartNew(async () =>
+                {
+                    await Materials.UpdateAll();
+                    BombPatch.SetStaticBombColor();
+                });
+
+                return true;
+            }
+
+#if V1_40_6
+            private static void PostfixCall(BeatmapKey beatmapKey, in GameplayModifiers gameplayModifiers)
+#else
+            private static void PostfixCall(BeatmapLevel beatmapLevel, BeatmapKey beatmapKey, in GameplayModifiers gameplayModifiers)
+#endif
             {
                 if (!Config.Enabled)
                 {
@@ -112,130 +128,90 @@ namespace NoteTweaks.Patches
 
 #if V1_40_6
                 AutoDisable =
-                    (MapHasRequirement(__instance.beatmapKey, "Noodle Extensions") &&
+                    (MapHasRequirement(beatmapKey, "Noodle Extensions") &&
                      Config.DisableIfNoodle) ||
-                    (MapHasRequirement(__instance.beatmapKey, "Vivify") &&
+                    (MapHasRequirement(beatmapKey, "Vivify") &&
                      Config.DisableIfVivify);
                 
                 UsesChroma = PluginManager.GetPluginFromId("Chroma") != null &&
-                             MapHasRequirement(__instance.beatmapKey, "Chroma", true);
+                             MapHasRequirement(beatmapKey, "Chroma", true);
 
                 _fixDots = true;
-                if (MapHasRequirement(__instance.beatmapKey, "Noodle Extensions"))
+                if (MapHasRequirement(beatmapKey, "Noodle Extensions"))
                 {
                     _fixDots = Config.FixDotsIfNoodle;
                 }
 #else
                 AutoDisable =
-                    (MapHasRequirement(__instance.beatmapLevel, __instance.beatmapKey, "Noodle Extensions") &&
+                    (MapHasRequirement(beatmapLevel, beatmapKey, "Noodle Extensions") &&
                      Config.DisableIfNoodle) ||
-                    (MapHasRequirement(__instance.beatmapLevel, __instance.beatmapKey, "Vivify") &&
+                    (MapHasRequirement(beatmapLevel, beatmapKey, "Vivify") &&
                      Config.DisableIfVivify);
-                
+
                 UsesChroma = PluginManager.GetPluginFromId("Chroma") != null &&
-                             MapHasRequirement(__instance.beatmapLevel, __instance.beatmapKey, "Chroma", true);
+                             MapHasRequirement(beatmapLevel, beatmapKey, "Chroma", true);
 
                 _fixDots = true;
-                if (MapHasRequirement(__instance.beatmapLevel, __instance.beatmapKey, "Noodle Extensions"))
+                if (MapHasRequirement(beatmapLevel, beatmapKey, "Noodle Extensions"))
                 {
                     _fixDots = Config.FixDotsIfNoodle;
                 }
 #endif
-                
+
                 _gameplayModifiers = gameplayModifiers;
                 _fixDots = (_fixDots && !gameplayModifiers.ghostNotes);
-                
+
                 Plugin.ClampSettings();
             }
             
-            // ReSharper disable once InconsistentNaming
-            internal static bool Prefix(StandardLevelScenesTransitionSetupDataSO __instance)
+            [HarmonyPatch]
+            internal class StandardLevelScenesTransitionSetupDataPatch
             {
-                if (!Config.Enabled)
-                {
-                    return true;
-                }
-                
-                Managers.Meshes.UpdateSphereMesh(Config.BombMeshSlices, Config.BombMeshStacks, Config.BombMeshSmoothNormals, Config.BombMeshWorldNormals);
-                
-                UnityMainThreadTaskScheduler.Factory.StartNew(async () =>
-                {
-                    await Materials.UpdateAll();
-                    BombPatch.SetStaticBombColor();
-                });
-                
-                return true;
-            }
-        }
+                // thanks BeatLeader
+                [UsedImplicitly]
+                private static MethodInfo TargetMethod() => AccessTools.FirstMethod(
+                    typeof(StandardLevelScenesTransitionSetupDataSO),
+                    m => m.Name == nameof(StandardLevelScenesTransitionSetupDataSO.Init) &&
+                         m.GetParameters().All(p => p.ParameterType != typeof(IBeatmapLevelData)));
 
-        [HarmonyPatch]
-        internal class MultiplayerLevelScenesTransitionSetupDataPatch
-        {
-            [UsedImplicitly]
-            [HarmonyPatch(typeof(MultiplayerLevelScenesTransitionSetupDataSO), "Init")]
-            // ReSharper disable once InconsistentNaming
-            internal static void Postfix(MultiplayerLevelScenesTransitionSetupDataSO __instance, in GameplayModifiers gameplayModifiers)
-            {
-                if (!Config.Enabled)
+                // ReSharper disable once InconsistentNaming
+                internal static void Postfix(StandardLevelScenesTransitionSetupDataSO __instance, in GameplayModifiers gameplayModifiers)
                 {
-                    UsesChroma = false;
-                    return;
-                }
-
 #if V1_40_6
-                AutoDisable =
-                    (MapHasRequirement(__instance.beatmapKey, "Noodle Extensions") &&
-                     Config.DisableIfNoodle) ||
-                    (MapHasRequirement(__instance.beatmapKey, "Vivify") &&
-                     Config.DisableIfVivify);
-                
-                UsesChroma = PluginManager.GetPluginFromId("Chroma") != null &&
-                             MapHasRequirement(__instance.beatmapKey, "Chroma", true);
-
-                _fixDots = true;
-                if (MapHasRequirement(__instance.beatmapKey, "Noodle Extensions"))
-                {
-                    _fixDots = Config.FixDotsIfNoodle;
-                }
+                    PostfixCall(__instance.beatmapKey, gameplayModifiers);
 #else
-                AutoDisable =
-                    (MapHasRequirement(__instance.beatmapLevel, __instance.beatmapKey, "Noodle Extensions") &&
-                     Config.DisableIfNoodle) ||
-                    (MapHasRequirement(__instance.beatmapLevel, __instance.beatmapKey, "Vivify") &&
-                     Config.DisableIfVivify);
-                
-                UsesChroma = PluginManager.GetPluginFromId("Chroma") != null &&
-                             MapHasRequirement(__instance.beatmapLevel, __instance.beatmapKey, "Chroma", true);
-
-                _fixDots = true;
-                if (MapHasRequirement(__instance.beatmapLevel, __instance.beatmapKey, "Noodle Extensions"))
-                {
-                    _fixDots = Config.FixDotsIfNoodle;
-                }
+                    PostfixCall(__instance.beatmapLevel, __instance.beatmapKey, gameplayModifiers);
 #endif
-                
-                _gameplayModifiers = gameplayModifiers;
-                Plugin.ClampSettings();
-            }
-            
-            [HarmonyPatch(typeof(MultiplayerLevelScenesTransitionSetupDataSO), "Init")]
-            // ReSharper disable once InconsistentNaming
-            internal static bool Prefix(MultiplayerLevelScenesTransitionSetupDataSO __instance)
-            {
-                if (!Config.Enabled)
-                {
-                    return true;
                 }
-                
-                Managers.Meshes.UpdateSphereMesh(Config.BombMeshSlices, Config.BombMeshStacks, Config.BombMeshSmoothNormals, Config.BombMeshWorldNormals);
-                
-                UnityMainThreadTaskScheduler.Factory.StartNew(async () =>
+
+                // ReSharper disable once InconsistentNaming
+                internal static bool Prefix()
                 {
-                    await Materials.UpdateAll();
-                    BombPatch.SetStaticBombColor();
-                });
-                
-                return true;
+                    return PrefixCall();
+                }
+            }
+
+            [HarmonyPatch]
+            internal class MultiplayerLevelScenesTransitionSetupDataPatch
+            {
+                [UsedImplicitly]
+                [HarmonyPatch(typeof(MultiplayerLevelScenesTransitionSetupDataSO), "Init")]
+                // ReSharper disable once InconsistentNaming
+                internal static void Postfix(MultiplayerLevelScenesTransitionSetupDataSO __instance, in GameplayModifiers gameplayModifiers)
+                {
+#if V1_40_6
+                    PostfixCall(__instance.beatmapKey, gameplayModifiers);
+#else
+                    PostfixCall(__instance.beatmapLevel, __instance.beatmapKey, gameplayModifiers);
+#endif
+                }
+
+                [HarmonyPatch(typeof(MultiplayerLevelScenesTransitionSetupDataSO), "Init")]
+                // ReSharper disable once InconsistentNaming
+                internal static bool Prefix()
+                {
+                    return PrefixCall();
+                }
             }
         }
 
